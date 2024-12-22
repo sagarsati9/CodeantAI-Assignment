2c135959045e638fd2b43571be1d1e804053da9e
sagar <sagarsati9@gmail.com>
2024-12-22 23:12:23 +0530

    updated view

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,40 +18,45 @@ const Home = () => {
             <SideBar className='fixed top-0 left-0 h-screen w-64 border-r-2 bg-white' />
             <div className='flex-1 p-4 overflow-auto bg-[#FAFAFA]'>
                 <div className=' flex flex-col border rounded-t-xl'>
-                    <div className='flex justify-between mt-4 items-center'>
+                    <div className='flex justify-between mt-2 items-center'>
                         <div className='flex flex-col m-2'>
                             <span className='font-bold text-xl'>Repositories</span>
                             <span className='text-sm font-light'>33 total repositories</span>
                         </div>
-                        <div className='flex'>
-                            <button className='border rounded-lg p-2 m-2 flex items-center'> <ArrowPathIcon className='w-4 m-2' />Refresh All</button>
-                            <button className='border bg-blue-400 text-white rounded-lg p-2 m-2 flex items-center'>  <PlusIcon className='w-4 m-2' />Add Repository</button>
+                        <div className='flex gap-2'>
+                            <button className='p-2 flex items-center text-xs px-4 gap-2 rounded-md border'> <ArrowPathIcon className='w-4' />Refresh All</button>
+                            <button className='flex text-xs items-center p-2 px-4 gap-2 bg-[#1570EF] text-white rounded-md'>  <PlusIcon className='w-4' />Add Repository</button>
                         </div>
                     </div>
                     <div>
-                        <label className='border flex rounded-md w-fit m-2'>
-                            <MagnifyingGlassIcon className='w-4 m-2' />
-                            <input type='text' placeholder='Search repositories' onChange={handleChange}>
+                        <label className='border flex rounded-md  gap-2 w-fit px-2 py-2 items-center m-2'>
+                            <MagnifyingGlassIcon className='w-4' />
+                            <input className='outline-none' type='text' placeholder='Search repositories' onChange={handleChange}>
                             </input>
                         </label>
                     </div>
                 </div>
-                {data.map((item) => (
+                {data.length >= 1 ? data.map((item) => (
                     <div className='flex flex-col border'>
-                        <div className='flex items-center'>
-                            <span className='m-2 p-2'>{item.title}</span>
-                            <span className='m-2 p-2 border rounded-xl bg-blue-300 text-white text-sm'>{item.type}</span>
+                        <div className='flex items-center gap-2'>
+                            <span className='p-2 font-medium'>{item.title}</span>
+                            <span className='bg-[#EFF8FF] border border-[#B2DDFF] text-primary text-xs px-2 rounded-full text-blue-600'>{item.type}</span>
                         </div>
-                        <div className='flex items-center'>
-                            <span className='flex items-center m-2 p-2 gap-2'>
+                        <div className='flex items-center gap-2 text-center'>
+                            <span className='flex items-center p-2 gap-2 font-light text-sm'>
                                 {item.language}
                                 <span className='bg-[#1570EF] p-1 rounded-full'></span>
                             </span>
-                            <span className='m-2 p-2 flex items-center'> <CircleStackIcon className='m-2 w-4' /> {item.size}</span>
-                            <span className='m-2 p-2'>{item.updatedAt}</span>
+                            <span className='p-2 flex items-center font-light text-sm'> <CircleStackIcon className='m-2 w-4' /> {item.size}</span>
+                            <span className=' p-2 font-light text-sm'>{item.updatedAt}</span>
                         </div>
                     </div>
-                ))}
+                )) : (
+                    <div className='flex flex-col border-b p-4 gap-3 text-gray-500 text-center'>
+                        Looks like there is no repository to show.
+                    </div>
+                )
+                }
             </div>
         </div>
     )
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -21,35 +21,35 @@ const SideBar = () => {
                 <ChevronDownIcon className='w-4'/>
             </span>
             <nav className='px-4 py-2'>
-               <a href='#' className='flex m-2 py-2 px-4 bg-blue-500 text-white rounded-xl'>
+               <a href='#' className='flex  items-center m-2 py-2 px-4 bg-[#1570EF] font-light text-white rounded-xl'>
                <span> <HomeIcon className='w-6'/></span>
-                <span className='ml-2'>Repositories</span>
+                <span className='ml-3'>Repositories</span>
                </a>
-               <a href='#' className='flex m-2 py-2 px-4'>
+               <a href='#' className='flex m-2 py-2 px-4 font-light'>
                 <span><CodeBracketIcon className='w-6'/></span>
                 <span className='ml-2'>AI Code review</span>
                </a>
-               <a href='#' className='flex m-2 py-2 px-4'>
+               <a href='#' className='flex m-2 py-2 px-4 font-light'>
                 <span><CloudIcon className='w-6'/></span>
                 <span className='ml-2'>Cloud Security</span>
                </a>
-               <a href='#' className='flex m-2 py-2 px-4'>
+               <a href='#' className='flex m-2 py-2 px-4 font-light'>
                 <span><BookOpenIcon className='w-6'/></span>
                 <span className='ml-2'>How To Use</span>
                </a>
-               <a href='#' className='flex m-2 py-2 px-4'>
-                <span><Bars4Icon className='w-6'/></span>
+               <a href='#' className='flex m-2 py-2 px-4 font-light'>
+                <span><Cog6ToothIcon className='w-6'/></span>
                 <span className='ml-2'>Settings</span>
                </a>
               
             </nav>
 
-            <div className='px-4 py-4'>
-                <a className='flex m-2 py-2 px-4'>
+            <div className='px-4 py-4 mt-20'>
+                <a href='#' className='flex m-2 py-2 px-4 font-light'>
                  <span><PhoneIcon className='w-6'/></span>
                  <span className='ml-2'>Support</span>
-                </a>
-                <a className='flex m-2 py-2 px-4'>
+                </a> 
+                <a href='#' className='flex m-2 py-2 px-4 font-light'>
                     <span><ArrowRightStartOnRectangleIcon className='w-6'/></span>
                     <span className='ml-2'>Logout</span>
                 </a>
